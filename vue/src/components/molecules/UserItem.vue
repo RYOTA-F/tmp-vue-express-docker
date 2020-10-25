8538307a251bc9b4d92c8f3a1b3725675f1f5af4
Ryota Fujishima <r.fujishima@comnico.jp>
2020-10-25 13:54:15 +0900

    UserItem add

---
+++ vue/src/components/molecules/UserItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="user-name">
+      {{ name }}
+    </div>
+
+    <div class="user-id">
+      {{ id }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class UserItem extends Vue {
+  @Prop({ type: Number, default: 0 })
+  id: number
+
+  @Prop({ type: String, default: 'null' })
+  name: string
+}
+</script>
--- vue/src/components/organisms/UserList.vue
+++ vue/src/components/organisms/UserList.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    {{ users }}
+    <div v-for="user in users" :key="user.id">
+      <UserItem :id="user.id" :name="user.name" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import { userModule } from '../../store/modules/user'
+import UserItem from '../molecules/UserItem.vue'
 
-@Component
+@Component({ components: { UserItem } })
 export default class UserList extends Vue {
   get users() {
     return userModule.apiUsers
